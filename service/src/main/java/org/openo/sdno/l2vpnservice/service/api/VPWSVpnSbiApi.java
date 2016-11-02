df870a51ecd83a77ae290600d314b1fcea157e63
MR-ZZC <zhaozhongchao@huawei.com>
2016-11-02 11:09:29 +0800

    L2VPNService L2AC missing l2Access in uniformsbi model.
    SDNO-827
    
    Change-Id: I33f315f5147f1c508e6f3beebc8b71b02a277e51
    Signed-off-by: MR-ZZC <zhaozhongchao@huawei.com>

--- service/src/main/java/org/openo/sdno/l2vpnservice/service/api/VPWSVpnSbiApi.java
+++ service/src/main/java/org/openo/sdno/l2vpnservice/service/api/VPWSVpnSbiApi.java
@@ -74,7 +74,7 @@ public class VPWSVpnSbiApi extends L2VpnSbiApi implements SbiApiService {
         String reqJson = JsonUtil.toJson(paras);
         reqJson = TranslateChecker.check(reqJson);
         LOGGER.info("Create l2vpn request is :" + reqJson);
-        final RestfulParametes restfulParametes = RestUtil.getRestfulParametes(JsonUtil.toJson(paras));
+        final RestfulParametes restfulParametes = RestUtil.getRestfulParametes(JsonUtil.toJson(reqJson));
         restfulParametes.putHttpContextHeader("X-Driver-Parameter", "extSysID=" + URLEncoderUtil.encode(controllerId));
 
         final RestfulResponse response = RestUtil.sendPostRequest(getProvisionUrl(), restfulParametes,
@@ -102,9 +102,9 @@ public class VPWSVpnSbiApi extends L2VpnSbiApi implements SbiApiService {
         final RestfulParametes restfulParametes = RestUtil.getRestfulParametes(JsonUtil.toJson(paras));
         restfulParametes.putHttpContextHeader("X-Driver-Parameter", "extSysID=" + URLEncoderUtil.encode(controllerId));
 
-        final RestfulResponse response = RestUtil.sendPutRequest(
-                MessageFormat.format(getModifyDescUrl(), URLEncoderUtil.encode(vpn.getUuid())), restfulParametes,
-                OwnerInfoThreadLocal.getHttpServletRequest());
+        final RestfulResponse response =
+                RestUtil.sendPutRequest(MessageFormat.format(getModifyDescUrl(), URLEncoderUtil.encode(vpn.getUuid())),
+                        restfulParametes, OwnerInfoThreadLocal.getHttpServletRequest());
         handleResponse(responsTranslator.tranlate(response), L2VpnSvcErrorCode.UPDATEDESC_CONTROLLER_FAIL);
         return L2VpnSbiApi.getVpnFromResponse(response);
     }
